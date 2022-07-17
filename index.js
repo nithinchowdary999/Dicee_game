c926b105236661ab5364b2d1c767af3234713c52
nithiin-999 <44894101+nithinchowdary999@users.noreply.github.com>
2022-07-17 12:22:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 var randomNumber1=Math.floor((Math.random() * 6)+ 1 );
 if(randomNumber1 ===1){
-  document.querySelector(".img1").setAttribute("src","images/dice1.png");
+  document.querySelector(".img1").setAttribute("src","dice1.png");
 }else if (randomNumber1 ===2) {
-  document.querySelector(".img1").setAttribute("src","images/dice2.png");
+  document.querySelector(".img1").setAttribute("src","dice2.png");
 
 }else if (randomNumber1 ===3) {
-  document.querySelector(".img1").setAttribute("src","images/dice3.png");
+  document.querySelector(".img1").setAttribute("src","dice3.png");
 
 }else if (randomNumber1 ===4) {
-  document.querySelector(".img1").setAttribute("src","images/dice4.png");
+  document.querySelector(".img1").setAttribute("src","dice4.png");
 
 }else if (randomNumber1 ===5) {
-  document.querySelector(".img1").setAttribute("src","images/dice5.png");
+  document.querySelector(".img1").setAttribute("src","dice5.png");
 
 }else{
-  document.querySelector(".img1").setAttribute("src","images/dice6.png");
+  document.querySelector(".img1").setAttribute("src","dice6.png");
 
 }
 var randomNumber2=Math.floor((Math.random() * 6)+ 1 );
 if(randomNumber2 ===1){
-  document.querySelector(".img2").setAttribute("src","images/dice1.png");
+  document.querySelector(".img2").setAttribute("src","dice1.png");
 }else if (randomNumber2 ===2) {
-  document.querySelector(".img2").setAttribute("src","images/dice2.png");
+  document.querySelector(".img2").setAttribute("src","dice2.png");
 
 }else if (randomNumber2 ===3) {
-  document.querySelector(".img2").setAttribute("src","images/dice3.png");
+  document.querySelector(".img2").setAttribute("src","dice3.png");
 
 }else if (randomNumber2 ===4) {
-  document.querySelector(".img2").setAttribute("src","images/dice4.png");
+  document.querySelector(".img2").setAttribute("src","dice4.png");
 
 }else if (randomNumber2 ===5) {
-  document.querySelector(".img2").setAttribute("src","images/dice5.png");
+  document.querySelector(".img2").setAttribute("src","dice5.png");
 
 }else{
   document.querySelector(".img2").setAttribute("src","images/dice6.png");
